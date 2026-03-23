c1a5bbc2e4bfa1d4603fece657920a0d8cb281be
AbdrahimMessadia <dzdzdzdzd095@gmail.com>
2026-03-23 22:30:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const adminRoutes = require('./routes/admin');
 const { checkSession } = require('./middleware/auth');
 
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // ── ضغط الاستجابات ──────────────────────────────────────────
